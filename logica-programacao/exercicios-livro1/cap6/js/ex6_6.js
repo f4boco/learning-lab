df0904ca53e33197341380fbaa0eb43ad0818263
Fabiano Olivr <ffabiano.olivr@gmail.com>
2026-06-11 06:21:02 -0300

    finalização do ex6.6 do livro1

--- logica-programacao/exercicios-livro1/cap6/js/ex6_6.js
+++ logica-programacao/exercicios-livro1/cap6/js/ex6_6.js
@@ -7,3 +7,52 @@
  *
  * autor: Fabiano O.
  */
+
+// referencia elementos do html
+const formulario = document.getElementById("formulario-senha");
+const inSenha = document.getElementById("inSenha");
+const outResultado = document.getElementById("outResultado");
+
+// adiciona "ouvinte de eventos" ao formulario
+formulario.addEventListener("submit", function (evento) {
+    // impede que a página recarregue e limpe o código js
+    evento.preventDefault();
+
+    // obtém o conteúdo do campo de entrada da senha
+    const senha = inSenha.value;
+
+    const erros = []; // vetor para receber os erros
+
+    // verifica se a senha possui entre 8 e 15 caracteres
+    if (senha.length < 8 && senha.length > 15) {
+        erros.push("possuir entre 8 e 15 caracteres");
+    }
+
+    // verifica se não possui nº
+    if (senha.match(/\d/g) == null) {
+        erros.push("possuir números (no mínimo 1)");
+    }
+
+    // verifica se não possui letras minúsculas
+    if (!senha.match(/[a-z]/g)) {
+        /* Se entrar nacondição, significa que o retorno fou null, que é !(false) */
+        erros.push("possuir letras minúsculas (no mínimo 1)");
+    }
+
+    // verifica se não possui letras maiúsculas ou se possui apenas 1
+    if (!senha.match(/[A-Z]/g) || senha.match(/[A-Z]/g).length == 1) {
+        erros.push("pussuir letras maiúsculas (no mínimo 2)");
+    }
+
+    // verifica se não possui símbolos ou "_"
+    if (!senha.match(/\W|_/)) {
+        erros.push("possuir símbolos (no mínimo 1)");
+    }
+
+    // se o vetor está vazio, significa que não foram encontrados erros
+    if (erros.length == 0) {
+        outResultado.innerText = "Ok! Senha Válida";
+    } else {
+        outResultado.innerText = "Erro... A senha deve " + erros.join(" - ");
+    }
+});
