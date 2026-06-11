1dc5d0e33100e3d868eee6cd67eaac64e16e327d
Fabiano Olivr <ffabiano.olivr@gmail.com>
2026-06-11 04:45:38 -0300

    criado ex6.6 do livro 1

---
+++ logica-programacao/exercicios-livro1/cap6/js/ex6_6.js
@@ -0,0 +1,9 @@
+/**
+ * esse script valida senha. para ser válida, uma senha deve possuir as
+ * seguintes regras de composição: a) possuir entre 8 e 15 caracteres; b)
+ * possuir, no mínimo, 1 número; c) possuir, no mínimo, 1 letra minúscula;
+ * d) possuir, no mínimo, 2 letras maiúsculas; e) possuir, no mínimo, 1
+ * símbolo.
+ *
+ * autor: Fabiano O.
+ */
