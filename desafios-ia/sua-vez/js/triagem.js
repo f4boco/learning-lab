78ac08f6e69ce4676cb7b83c76a094e49a46ec20
Fabiano Olivr <ffabiano.olivr@gmail.com>
2026-06-12 06:05:51 -0300

    criado a verificação das entradas da triagem

---
+++ desafios-ia/sua-vez/js/triagem.js
@@ -0,0 +1,54 @@
+/**
+ * esse script é responsável pela dinâmica de triagem:
+ * capturar os dados do html, tratar e adicionar ao vetor.
+ * autor: Fabiano O.
+ */
+
+// referencia os elementos do html de "triagem.html"
+const formularioTriagem = document.getElementById("formulario-triagem");
+const inNome = document.getElementById("nome");
+const inCpf = document.getElementById("cpf");
+const inTelefone = document.getElementById("telefone");
+const inPreferencia = document.getElementById("preferencial");
+
+// vetor para guardar as pesoas da fila
+const fila = [];
+
+// adiciona "ouvinte de eventos" ao formulario
+formularioTriagem.addEventListener("submit", function (e) {
+    // impede que a página recarregue e limpe os códigos js
+    e.preventDefault();
+
+    // obter os conteúdos dos elementos de entrada
+    const nome = inNome.value.trim();
+    const cpf = inCpf.value.trim();
+    const telefone = inTelefone.value.trim();
+
+    // verifica se o nome é válido (precisa de nome e sobrenome)
+    if (!nome.match(" ")) { // se a busca por " " retornar null (false), não tem sobrenomes
+        alert("Digite o Nome Completo...");
+        inNome.focus();
+        return;
+    }
+    // verica se o cpf é válido
+    if (!cpf.match(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)) {
+        // Engenharia do padrão de CPF:
+        // \d{3} -> 3 números grudados      \.     -> seguidos por um ponto
+        // \d{3} -> 3 números grudados      \.     -> seguidos por um ponto
+        // \d{3} -> 3 números grudados       -     -> seguidos por um ifen
+        // \d{2} -> seguidos por mais 2 números grudados    (^ e $ São âncoras)
+        alert("CPF Inválido...");
+        inCpf.focus();
+        return;
+    }
+    // verifica se foi digitado algum telefone
+    if (telefone != "") {
+        // verifica se o telefone é válido
+        if (!telefone.match(/^\d{2} \d{5}-\d{4}$/)) {
+            alert("Telefone Inválido...");
+            inTelefone.focus();
+            return;
+        }
+    }
+
+});
