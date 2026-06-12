c620cc11b45d69c9aea754955d9d7d9485f6e060
Fabiano Olivr <ffabiano.olivr@gmail.com>
2026-06-12 05:10:47 -0300

    criado o dates.js

--- desafios-ia/sua-vez/js/dates.js
+++ desafios-ia/sua-vez/js/dates.js
@@ -2,3 +2,14 @@
  * esse script é resposável pela manipulação da data
  * autor: Fabiano O.
  */
+
+
+// referencia o elemento html de "data-atual"
+const dataAtual = document.getElementById("data-atual");
+
+// obter data atual e ...
+const hoje = new Date();
+const dataBR = hoje.toLocaleDateString('pt-BR'); // formatar data para o Brasil (DD/MM/AAAA)
+
+// atualizar o texto do elemento html
+dataAtual.innerText = dataBR;
