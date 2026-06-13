516bf7d496b006391ab568fd1315c630131f8ffc
Fabiano Olivr <ffabiano.olivr@gmail.com>
2026-06-13 05:59:38 -0300

    implemetado a sensura com * em todas as letras

--- logica-programacao/exercicios-livro1/cap6/js/exIA_1.js
+++ logica-programacao/exercicios-livro1/cap6/js/exIA_1.js
@@ -3,3 +3,31 @@
  * em um Chat
  * autor: Fabiano O.
  */
+
+function exibirChat() {
+    // referencia os elementos de entrada e saída
+    const inMensagem = document.getElementById("inMensagem");
+    const outChat = document.getElementById("outChat");
+
+    // obter o conteúdo do campo de mensagem
+    let mensagem = inMensagem.value.trim();
+
+    // verifica se a entrada é válida
+    if (mensagem == "") {
+        alert("Digite uma mensagem...");
+        inMensagem.focus();
+        return;
+    }
+
+    // obtém as palavras que devem sensurar
+    const palavrasSensuradas = mensagem.match(/bobo|feio|chato/gi);
+
+    // percorre o vetor de palavras sensuradas
+    for (let sensurada of palavrasSensuradas) {
+        const asteriscos = "*".repeat(sensurada.length);
+        mensagem = mensagem.replace(sensurada, asteriscos);
+    }
+    console.log(mensagem);
+}
+// referencia o btEnviar e vincula function exibitChat ao evento click
+document.getElementById("btEnviar").addEventListener("click", exibirChat);
