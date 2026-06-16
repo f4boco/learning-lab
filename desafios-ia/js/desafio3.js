03af47754a798f839710c43fc1bfcbc686e9889a
Fabiano Olivr <ffabiano.olivr@gmail.com>
2026-06-16 05:48:31 -0300

    Ajuste na exibição de vetores para vetor vazio

--- desafios-ia/js/desafio3.js
+++ desafios-ia/js/desafio3.js
@@ -11,19 +11,19 @@ exibirProdutos();
 function gerarProdutos() {
     const listaProdutosTemp = []; // receberá os objetos aleatórios
     const categoriasProdutos = ["Consumer Electronics", "Smart Home", "Periféricos e Hardware de Computação"];
-    
+
     // defin, 6 produtos para cada categoria, em órdem
     const baseProdutos = ["iPhone 15 Pro", "MacBook Air M3", "Samsung Galaxy Tab S9", "Apple Watch Series 9",
         "Sony WH-1000XM5", "iPad Pro M4", "Amazon Echo Dot 5ª Geração", "Philips Hue Smart Bulb", "iRobot Roomba j7",
         "Fechadura Digital Yale YRD 256", "Smart TV LG OLED Evo", "Google Nest Cam", "NVIDIA GeForce RTX 4070",
-        "Monitor Alienware 34 Curved", "Teclado Mecânico Logitech G915","Roteador ASUS ROG Rapture GT6", "SSD Kingston Fury Renegade",
+        "Monitor Alienware 34 Curved", "Teclado Mecânico Logitech G915", "Roteador ASUS ROG Rapture GT6", "SSD Kingston Fury Renegade",
         "Mouse Logitech G Pro X Superlight 2"];
 
     // percorre o vetor de categorias
     for (let i = 0; i < categoriasProdutos.length; i++) {
         // gera um nº aleatório entre 5 e 20 para definir a quantidade de produtos da categoria i
         const qtdProdutos = gerarNumeros(5, 20);
-        
+
         let vControle = 0;
         while (vControle < qtdProdutos) {
             const posicaoProduto = gerarNumeros(i * 6, (i + 1) * 6);
@@ -60,21 +60,26 @@ function exibirProdutos(vetor = listaProdutos) {
     // limpa o conteúdo do elemento
     outProdutos.innerHTML = "";
 
-    // percorre o vetor
-    for (produto of vetor) {
-        // cria um elemento html para o produto
-        const cartaoProduto = document.createElement("div");
+    // verifica se o vetor está vazio
+    if (vetor.length == 0) {
+        outProdutos.innerHTML = `<p>Hummm, não há produtos</p>`;
+    } else {
+        // percorre o vetor
+        for (produto of vetor) {
+            // cria um elemento html para o produto
+            const cartaoProduto = document.createElement("div");
 
-        // adiciona a classe de estilização ao cartão
-        cartaoProduto.classList.add("cartao");
+            // adiciona a classe de estilização ao cartão
+            cartaoProduto.classList.add("cartao");
 
-        // adiciona o html que monta a estrutura de exibição do produto
-        cartaoProduto.innerHTML = `
-            <p>${produto.nome} - ${produto.preco}</p>
-            <small>${produto.categoria}</small>
-        `;
+            // adiciona o html que monta a estrutura de exibição do produto
+            cartaoProduto.innerHTML = `
+                <p>${produto.nome} - ${produto.preco}</p>
+                <small>${produto.categoria}</small>
+            `;
 
-        // coloca o elemento criado dentro do elemento de saída
-        outProdutos.appendChild(cartaoProduto);
+            // coloca o elemento criado dentro do elemento de saída
+            outProdutos.appendChild(cartaoProduto);
+        }
     }
 }
