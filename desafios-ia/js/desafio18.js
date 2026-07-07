6fc81f9b6c8446b976400b336ee96f7e2633f442
Fabiano Olivr <ffabiano.olivr@gmail.com>
2026-07-07 04:53:18 -0300

    Finalizado o processo gerar-opcoes

---
+++ desafios-ia/js/desafio18.js
@@ -0,0 +1,46 @@
+/**
+ * esse script gera as opções de meses...
+ * autor: Fabiano O.
+ */
+
+// referencia o elemento de Entrada/Saída
+const inMes = document.getElementById("inMes");
+
+// vetor de colaboradores
+const colaboradores = [
+  { nome: "Ana Silva", dataNascimento: new Date("1995-07-14") },
+  { nome: "Carlos Souza", dataNascimento: new Date("1988-03-22") },
+  { nome: "Beatriz Costa", dataNascimento: new Date("2000-07-05") },
+  { nome: "David Oliveira", dataNascimento: new Date("1992-12-25") }
+];
+
+// bloco de Execução
+{
+    // executa a função para gerar as opções do select
+    gerarOpcoes();
+}
+
+// função que gera as opções do select inMes
+function gerarOpcoes() {
+    // meses de opções
+    const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto",
+        "Setembro", "Outubro", "Novembro", "Dezembro"];
+    const mesAtual = new Date().getMonth(); // Obtém o mês atual (0-11)
+
+    // percorre todo o vetor meses
+    for (let i = 0; i < meses.length; i++) {
+        // obtém o mes correspondente ao loop atual
+        const mes = meses[i];
+
+        // cria e monta o elemento html de opção
+        const novaOpcao = document.createElement("option");
+        novaOpcao.value = mes.slice(0, 2); // define apenas as três primeiras letras
+        novaOpcao.text = mes;
+        // verifica se é o mês atual
+        if (i === mesAtual)
+            novaOpcao.selected = true;
+
+        // adiciona a opção criada o select
+        inMes.appendChild(novaOpcao);
+    }
+}
--- desafios-ia/processos/desafio_18/gerar-opcoes.md
+++ desafios-ia/processos/desafio_18/gerar-opcoes.md
@@ -6,11 +6,24 @@ Obter o Elemento html de entrada, gerar as opções de meses e definir o mês at
 
 ## Início
 
-O botão `Agrupar`é clicado
+O Site é Carregado
 
 ## Fluxo
 
-1. 
+1. Referenciar o elemento html `inMes`;  
+2. Criar uma função `listarOpcoes`:  
+    2.1 Criar um vetor `meses` com todos os meses do ano;  
+    2.2 Obter o `mesAtual` com `new Data().getMonth()`;  
+    2.3 Percorrer todos os elementos do vetor `meses` com `for()`;  
+    2.4 Para cada `i`:  
+        2.4.1 Criar um novo Elemento html `novaOpcao` que recebe uma `<option>`
+        2.4.2 Definir o `novaOpcao.value` como as três primerias letras de `meses[i]`;  
+        2.4.3 Definir o `novaOpcao.text` `como meses[i]`;  
+        DECISÃO:  
+        2.4.4 Verifica se `meses[i] === mesAtual`;  
+            SIM:  
+            2.4.4.1 Definir `novaOpcao.selected = true`;  
+        2.4.5 Adicionar `novaOpcao` a `inMes`;  
 
 ## Regras de Negócio
 
