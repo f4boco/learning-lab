cb45e701180b8071d61b7b0f2d0d1fc3ca15bf2a
Fabiano Olivr <ffabiano.olivr@gmail.com>
2026-07-08 13:15:37 -0300

    Finalizado a Criptografia

--- logica-programacao/exercicios-livro1/cap6/js/ex6_a.js
+++ logica-programacao/exercicios-livro1/cap6/js/ex6_a.js
@@ -4,3 +4,39 @@
  * 
  * autor: Fabiano O.
  */
+
+
+// função que criptografa a mensagem
+function criptografarMensagem() {
+    // referencia o elemento html de entrada
+    const inMensagem = document.getElementById("inMensagem");
+    const mensagem = inMensagem.value.trim(); // obtém o conteúdo e remove os espaços dos extremos
+
+    // verifica se a entrada é uma mensagem válida
+    if (mensagem === ""  || mensagem.length < 2) {
+        alert("Mensagem inválida ou muito curta.");
+        inMensagem.value = "";
+        inMensagem.focus();
+        return;
+    }
+
+    // cria uma nova string com apenas os carcteres dos index pares
+    const mensagemCripto = 
+        mensagem.split("")
+        .filter((caractere, i) => {
+            if (i % 2 === 0) {
+               return caractere;
+            }
+        });
+    
+    // chama a função de exibir mensagem
+    exibirMensagem(mensagemCripto.join(""), "Criptografada");
+}
+// referencia o btCriptografar
+const btCriptografar = document.getElementById("btCriptografar");
+btCriptografar.addEventListener("click", criptografarMensagem); // associa a função criptografarMensagem ao evento click
+
+// função que exibe a mensagem
+function exibirMensagem(mensagem, criptografadaSN) {
+    document.getElementById("outSaida").innerHTML = `Mensagem ${criptografadaSN}:\n${mensagem}`;
+}
