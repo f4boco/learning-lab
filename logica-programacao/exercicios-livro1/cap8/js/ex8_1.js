4b9dbed7c010e999d4b96077dd10513bd4118627
f4boco <ffabiano.olivr@gmail.com>
2026-09-17 04:52:14 -0300

    feat: criado a função de verificar informações do localStorage

--- logica-programacao/exercicios-livro1/cap8/js/ex8_1.js
+++ logica-programacao/exercicios-livro1/cap8/js/ex8_1.js
@@ -5,6 +5,20 @@ const opcoesClubes = document.querySelectorAll(
     `input[name="clube"]`
 );
 
+const KEY_LOCAL_STORAGE = "clube";
+
+function setLocalStorage(key, value) {
+    localStorage.setItem(key, value);
+}
+
+function getLocalStorage(key) {
+    return localStorage.getItem(key);
+}
+
+function marcarRadio(idRadio) {
+    document.querySelector(`#${idRadio}`).checked = true;
+}
+
 function trocarClube() {
     const clubeSelecionado = document.querySelector(
     `input[name="clube"]:checked`
@@ -14,8 +28,29 @@ function trocarClube() {
     imgClube.src = "imagens/" + clubeSelecionado.toLowerCase() + ".webp";
     imgClube.className = "exibe";
     imgClube.alt = `Simbolo do ${clubeSelecionado}`;
+
+    setLocalStorage(KEY_LOCAL_STORAGE, clubeSelecionado);
+}
+
+function verificarClube() {
+    const clube = getLocalStorage(KEY_LOCAL_STORAGE);
+    if (clube) {
+        switch (clube) {
+            case "Brasil" :
+                marcarRadio(`rb${clube}`);
+                break;
+            case "Pelotas" :
+                marcarRadio(`rb${clube}`);
+                break;
+            case "Farroupilha" :
+                marcarRadio(`rb${clube}`);
+        }
+        trocarClube();
+    }
 }
 
 opcoesClubes.forEach(clube => {
     clube.addEventListener("change", trocarClube);
 });
+
+verificarClube();
